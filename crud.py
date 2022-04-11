0ad6afd0d40b72d8ae4f0ce7f23ba5b425d65e2b
Pablo Luiz <80119424+devpabloluiz@users.noreply.github.com>
2022-04-11 14:57:16 -0300

    Update crud.py

--- crud.py
+++ crud.py
@@ -3,7 +3,7 @@ import mysql.connector
 conexao = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='2426603512',
+    password='********',
     database='vendas',
 )
 cursor = conexao.cursor()
